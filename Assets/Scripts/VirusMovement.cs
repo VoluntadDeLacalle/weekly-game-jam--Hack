0b11660a8c33c7701ddd45e43357eca376b85814
William Gavin Lacalle <digitalbros7@gmail.com>
2020-03-02 22:15:29 -0600

    Initial Commit

---
+++ Assets/Scripts/VirusMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusMovement : MonoBehaviour
+{
+    public float speed = 3;
+
+    void Update()
+    {
+        transform.position += ((transform.forward * speed) * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Firewall")
+        {
+            print("Got em!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/VirusSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusSpawner : MonoBehaviour
+{
+    public float spawnTime;
+    public float spawnBuffer;
+
+    public GameObject virus;
+    public GameObject firewall;
+    private BoxCollider spawnCube;
+
+    void Awake()
+    {
+        spawnCube = GetComponent<BoxCollider>();
+    }
+
+    void Start()
+    {
+        //print(spawnCube.bounds.min + " " + spawnCube.bounds.max);
+        spawnTime = Time.time;
+    }
+
+    void Update()
+    {
+
+        if (Time.time - spawnTime >= spawnBuffer)
+        {
+            spawnVirus();
+            spawnTime = Time.time;
+        }
+    }
+
+    public void spawnVirus()
+    {
+        GameObject temp = Instantiate(virus);
+        temp.transform.position = new Vector3(transform.position.x, .5f, Random.Range(spawnCube.bounds.min.z, spawnCube.bounds.max.z));
+        temp.transform.forward = new Vector3(-1, 0, 0);
+    }
+}
